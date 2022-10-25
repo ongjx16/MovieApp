8b70b71ff4e3d1253621e8e9a7c0f8848ef72152
ongjx16 <83200360+ongjx16@users.noreply.github.com>
2022-10-25 22:32:46 +0800

    main app

---
+++ MainApp.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class MainApp{
+    public static void main (String[] args){
+        int identity;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Who are you?\n");
+        System.out.println("1. Admin\n");
+        System.out.println("2. Movie Goer\n");
+        System.out.println("3. Exit\n\n");
+        System.out.println("Enter a number of your choice: ");
+        identity = scan.nextInt();
+
+        if (identity == 1){
+            System.out.println("i am admin\n");
+            //log in as admin
+        }
+        else if (identity ==2){
+            System.out.println("i am movie goer\n");
+        }
+        else{
+            return;
+        }
+    
+    
+    }
+}
