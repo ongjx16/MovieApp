de5b784bd5ac8fcbc28ce3614062427fe3249044
htuttttt <sawthidahtut@gmail.com>
2022-11-03 00:26:53 +0800

    added stuff to Seat

--- Seat.java
+++ Seat.java
@@ -1,19 +1,25 @@
 public class Seat {
-    private String seatId;
+    private int seatId;
     private boolean seatStatus = false; // false means unoccupied, true means occupied
 
-    public Seat(String seatId){
+    public Seat(int seatId){
         this.seatId = seatId;
     }
 
+    public int getSeatId(){
+        return seatId;
+    }
+
+    // to check if the seat is occupied
     public boolean isOccupied(){
         return seatStatus;
     }
 
+    // assign if seat empty after checking w isOccupied()
     public void assign(){
         this.seatStatus = true;
     }
-
+    
     public void unassign(){
         this.seatStatus = false;
     }
