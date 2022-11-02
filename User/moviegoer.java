0eee1799fd4d490ff9d6dbe45c1515e9f9825898
ShubhangamPrasad <113502034+ShubhangamPrasad@users.noreply.github.com>
2022-11-02 23:53:33 +0800

    all done except processing seats

--- User/moviegoer.java
+++ User/moviegoer.java
@@ -1,10 +1,7 @@
-import java.util.*;
 
 // need username and passwords
 // need to figure out the function that pulls the booking history from the database
 // need to figure out the function that pulls overall ratings thus far from the database
-
-
 package User;
 
 public class moviegoer {
@@ -12,32 +9,36 @@ public class moviegoer {
     private String name;
     private int phoneNumber;
     private String email;
-    private int age;
+    private int myAge;
     private String [] bookingHistory;
     private int overallRatings;
 
-    public moviegoer(String name, int phoneNumber, String email, int age){
+    public moviegoer(String name, int phoneNumber, String email, int myAge){
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
-        this.age = age;
-        this.transactionID = "";
+        this.myAge = myAge;
     }
 
     public String getName (){
         return name;
     }
     public int getAge (){
-        return age;
+        return myAge;
     }
     public String[] showBookingHistory (){
         return bookingHistory;
+    }
+
+    public void setTransactionID(String x){
+        this.transactionID = x;
     }
     public void addBookingHistory(String a){
         // idk what to add here
     }
     public int getOverallRatings(){
         // idk what to add here
+        return 1;
     }
 }
 
