1e63590f3fb2ff19c776fdbb5a49ae26796fca7e
ShubhangamPrasad <113502034+ShubhangamPrasad@users.noreply.github.com>
2022-11-02 21:35:47 +0800

    Added basic attributes, functions, and comments to address

--- User/moviegoer.java
+++ User/moviegoer.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
+// need username and passwords
+// need to figure out the function that pulls the booking history from the database
+// need to figure out the function that pulls overall ratings thus far from the database
+
+
 package User;
 
 public class moviegoer {
-    
+    private String transactionID;
+    private String name;
+    private int phoneNumber;
+    private String email;
+    private int age;
+    private String [] bookingHistory;
+    private int overallRatings;
+
+    public moviegoer(String name, int phoneNumber, String email, int age){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.age = age;
+        this.transactionID = "";
+    }
+
+    public String getName (){
+        return name;
+    }
+    public int getAge (){
+        return age;
+    }
+    public String[] showBookingHistory (){
+        return bookingHistory;
+    }
+    public void addBookingHistory(String a){
+        // idk what to add here
+    }
+    public int getOverallRatings(){
+        // idk what to add here
+    }
 }
+
